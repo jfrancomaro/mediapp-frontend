f6b8f5ef4eb11d98a54fba284e30e1abc2e859fa
francomartinezroncal <jean.martinez@unmsm.edu.pe>
2020-12-19 20:05:51 -0500

    Finalizacion del frontend

---
+++ src/app/pages/signos/signos.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { SignosComponent } from './signos.component';
+
+describe('SignosComponent', () => {
+  let component: SignosComponent;
+  let fixture: ComponentFixture<SignosComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SignosComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SignosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
